6da2fd079d71000c1817b316c611c72034d51823
Livin Mathew <livinmathew99@gmail.com>
2018-10-07 22:42:19 +0530

    Ignored setup.js

--- arch/src/config/setup.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  const ENV_VARIABLES = require("./config.json").env;
-  Object.keys(ENV_VARIABLES).forEach(key => {
-    process.env[key] = ENV_VARIABLES[key];
-  });
-  console.info(`Exported Environment`);
-};
